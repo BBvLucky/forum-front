cbbb4b163b6fe5c500b4097fe3f52afc919feabc
BBvLucky <35274237+BBvLucky@users.noreply.github.com>
2019-01-12 21:18:38 +0300

    немножко стилизовал страницу 404

--- src/views/Error404.vue
+++ src/views/Error404.vue
@@ -1,5 +1,22 @@
 <template>
-    <div class="error-page">
-        <h1>Error 404. The page doesn't exist or it was deleted</h1>
-    </div>
+  <div class="error-page">
+    <h1 class="error-page-caption">404</h1>
+    <p class="error-page-text">The page doesn't exist or it was deleted</p>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'Error404',
+};
+</script>
+
+<style lang="scss">
+.error-page-caption {
+  font-size: 70px;
+}
+.error-page-text {
+    font-size: 30px;
+    font-weight: bold;
+}
+</style>
